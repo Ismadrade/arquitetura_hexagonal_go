a02a1c4bd56051f371c9c6f70780b7a3a241e3fd
Ismael Andrade <maelandrade@yahoo.com.br>
2022-11-04 20:38:00 -0300

    Feat: Add cobra library

--- main.go
+++ main.go
@@ -1,18 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-	db2 "github.com/Ismadrade/go-hexagonal/adapters/db"
-	"github.com/Ismadrade/go-hexagonal/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/Ismadrade/go-hexagonal/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "db.sqlite")
-	productDbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-	product, _ := productService.Create("Product Examplo", 30)
-
-	productService.Enable(product)
-
+	cmd.Execute()
 }
